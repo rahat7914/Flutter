e5063a489d20f58e3a9f3d8ac93a60f0f09b1060
km_rahat <k.m.rahat@gmail.com>
2021-05-03 13:30:37 +0600

    Add: State+Button actions

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,36 @@ void main() => runApp(MyApp());
 //MyFarmlyApp is a class that inherits baseclass StatelessWidget
 //MyFarmlyApp inherits the properties of SW class so it can turn into SW which futter understands
 @override
-class MyApp extends StatelessWidget {
+//class MyApp extends Statelesswidget : Here the states of widget can not change
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
+  }
+}
+
+//We have to make DART understand that MyAppState belongs to the MyApp class. Two connections need to be made
+//First add <MyApp> [pointer] after State [generic class] that tells DART that this State belongs to MyApp class
+class MyAppState extends State<MyApp> {
+//Tyring to display questions dynamically
+  var questionIndex = 0;
+
+  void answerQuestion() {
+    // We use setState to tell Dart we are about to change the state
+    // widget associated with that change only renders
+    setState(() {
+      // print('Chosen this answer');
+      questionIndex = questionIndex + 1;
+    });
+
+    print(questionIndex);
+  }
+
+  void answerAnotherQuestion() {
+    print('Another question is chosen');
+  }
+
   //build method will take in the object 'context' automatically
   //BuildContext is a type annotation
   //build method is called whenever DART needs to show something on the screen.
@@ -27,7 +56,9 @@ class MyApp extends StatelessWidget {
     //add an backslash (\) while using (') in words. Example of List
     var questions = [
       'What\'s your favorite color?',
-      'What\'s your favorite animal?'
+      'What\'s your favorite animal?',
+      'How many variables can you take into account?',
+      'Does learning flutter benefit you?'
     ];
     //MaterialApp() is a widget out here
     //Base setup to turn combination of widgets into a real app that can be rendered
@@ -44,26 +75,48 @@ class MyApp extends StatelessWidget {
         //body: Text('This is going to be Farmly app'),
         body: Column(
           children: [
-            Text('This is text within a column'),
+            Row(
+              children: [
+                //Text(questions.elementAt(0)),
+                Text(questions[questionIndex]),
+              ],
+            ),
             //Raised button is ElevatedButton
             Padding(
               padding: EdgeInsets.all(10),
             ),
-            ElevatedButton(
-              onPressed: null,
-              child: Text('Answer 01'),
-            ),
-            ElevatedButton(
-              onPressed: null,
-              child: Text('Answer 02'),
-            ),
-            ElevatedButton(
-              onPressed: null,
-              child: Text('Answer 03'),
-            ),
-            ElevatedButton(
-              onPressed: null,
-              child: Text('Answer 04'),
+            Column(
+              children: [
+                Row(
+                  children: [
+                    ElevatedButton(
+                      //onPressed takes a function that takes no argument () and return type is void
+                      //add function in the same class to make it a stand alone unit
+                      //with paranthesis dart is executiing the funtion at runtime and trying to get a value. but void so no value
+                      //In order to make onPressed work we have to pass a pointer of that method by removing the paranthesis. This way the method only executes when onPressed gets activated upon click
+                      onPressed: answerQuestion,
+                      child: Text('Answer 01'),
+                    ),
+                    ElevatedButton(
+                      //answerQuestion is a named funtion because it has a same. Not anonymous function
+                      onPressed: answerAnotherQuestion,
+                      child: Text('Answer 02'),
+                    ),
+                    ElevatedButton(
+                      //Example of anonymous funtion () => single line. We use this if I dont want to call this function anywhere else.
+                      onPressed: () => print('Answer 03 is chosen'),
+                      child: Text('Answer 03'),
+                    ),
+                    ElevatedButton(
+                      //Example of anonymous function () {funtion body} we use it, when this funtion is element specific
+                      onPressed: () {
+                        print('Answer 04 is chosen');
+                      },
+                      child: Text('Answer 04'),
+                    ),
+                  ],
+                )
+              ],
             ),
           ],
         ),
